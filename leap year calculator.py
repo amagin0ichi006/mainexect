3bf4230c3073c3f085285ce67f7c6d26d58a1d6b
amagin0ichi006 <146825206+amagin0ichi006@users.noreply.github.com>
2023-10-11 06:02:09 -0700

    Add files via upload

---
+++ leap year calculator.py
@@ -0,0 +1,10 @@
+print("welcome to leap year calclator!!!")
+year = int(input("pls input your year"))
+if year % 4 == 0:
+    print("it is a leap year")
+if year % 100 == 0:
+    print("it is not a leap year")
+else:
+    print("it is not a leap year") 
+    
+    
